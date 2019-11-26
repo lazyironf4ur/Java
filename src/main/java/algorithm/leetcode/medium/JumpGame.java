3abdfdf3e47328d2e31e7b2d7ea51a8a960accb3
Lazy1ron <lazy1ron.s3efun@gmail.com>
2019-11-26 16:14:42 +0800

    优化JumpGame解法

--- src/main/java/algorithm/leetcode/medium/JumpGame.java
+++ src/main/java/algorithm/leetcode/medium/JumpGame.java
@@ -39,6 +39,29 @@ public class JumpGame {
 
     }
 
+
+    // 第二种解法：使用迭代储存最近最优解
+    // 执行用时 :2 ms在所有 java 提交中击败了75.18%的用户 = = 内存消耗 :38.8 MB在所有 java 提交中击败了94.43%的用户
+
+
+    public static boolean canJump2(int[] nums) {
+        // 原始长度
+        int count = nums.length;
+        if (count == 1) return true;
+        boolean flag3 = false;
+        for (int i = count -2; i > -1; i--) {
+            // 需要满足表达式: x[i] >= count - i - 1;
+            if (nums[i] >= count - i - 1) {
+                flag3 = true;
+                count = i + 1;  // 动态改变数组长度
+            } else {
+                flag3 = false;
+                continue;
+            }
+        }
+        return flag3;
+    }
+
     public static void main(String[] args) {
         //int[] nums = new int[]{1,5,8,9,10,12,16,18};
         //System.out.println(Arrays.toString(Arrays.copyOfRange(nums, 0, 5)));
